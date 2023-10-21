d259cac8ac3a411d018f8553feb9a3cf3872d764
GerberContreras <gerberconsul@gmail.com>
2023-10-20 23:11:09 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+let cors = require("cors");
+app.use(cors());
+//nos ayuda a analizar el cuerpo de la solicitud POST
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//cargamos el archivo de rutas
+app.use(require('./routes/tienda'));
+
+app.listen(process.env.PORT||3300,() => {
+    console.log("Servidor corriendo en el puerto 3300");
+});
+
+module.exports = app;
